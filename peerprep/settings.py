009002adc2894dcb40cfd5b8caf962d4d0659d7a
Laiba-muzammal <laibamuzammal7@gmail.com>
2025-08-12 10:08:55 +0500

    chore(security): move SECRET_KEY to .env using python-decouple

--- peerprep/settings.py
+++ peerprep/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$k3h(&rk0(__o6=nns9ih$qjn_yansp3zis)k8^5rxl%*6t8e'
 
+
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
